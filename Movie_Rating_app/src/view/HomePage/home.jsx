da2a3d4bd3dae64847517400eb3730d8828b339a
LingJie Pan <xixixihahaha27@gmail.com>
2025-10-14 01:18:16 -0400

    added more styling

--- Movie_Rating_app/src/view/HomePage/home.jsx
+++ Movie_Rating_app/src/view/HomePage/home.jsx
@@ -3,6 +3,7 @@ import "../Component/MovieDetailCard/MovieDetailCard.css"
 import NavBar from "../Component/Navbar.jsx"
 import {Link} from 'react-router-dom';
 import {movies} from "../MovieDetailPage/movies.js";
+import { upcomingMovies } from "../MovieDetailPage/movies.js";
 
 function Home(){
   const EEAAO = movies.find((m) => m.id === 2);
@@ -32,8 +33,24 @@ function Home(){
             <Link to={`/movie/${movie.id}`}>
                <div className="poster" aria-hidden="true"><img style={{width:"200px", height:"300px"}}src={movie.poster}/></div>
                <h3 style={{width:"200px"}}>{movie.title}</h3>
-                <p class="meta">2010</p>
+                <p class="meta">{movie.DOR}</p>
               <p class="stars">★ ★ ★ ★ ☆</p>
+            </Link>
+        </div>
+      ))}
+      </section>
+
+       <section class="hero">
+          <h2>Coming soon!</h2>
+        </section>
+
+      <section class="movies-row">
+         {upcomingMovies.map((movie) => (
+            <div key={movie.id} className="card">
+            <Link to={`/movie/coming-soon/${movie.id}`}>
+               <div className="poster" aria-hidden="true"><img style={{width:"200px", height:"300px"}}src={movie.poster}/></div>
+               <h3 style={{width:"200px"}}>{movie.title}</h3>
+                <p class="meta">{movie.DOR}</p>
             </Link>
         </div>
       ))}
